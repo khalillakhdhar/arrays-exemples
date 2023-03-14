5dabd52eefeed3e4576c87204b2a656ac6b96e8a
khalil <khalillakhdharatc@gmail.com>
2023-03-14 13:53:26 +0100

    Initial commit

---
+++ demoTable.js
@@ -0,0 +1,7 @@
+tab=[2,"adel",2.5,true];
+//console.log(tab);
+console.log(tab[2]);
+for(let i=0;i<tab.length;i++)
+{
+    console.log(tab[i]);
+}
---
+++ manipTable.js
@@ -0,0 +1,7 @@
+t=[1,3,4,8,6,22,31];
+for(var i=0;i<t.length;i++)
+{
+    if(t[i]%2==1)
+    console.log(t[i]);
+    
+}
